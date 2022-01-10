85d78bbf66ba0a6c80edf734dfcc721d44165180
vukadinovic936 <milosvuk99@gmail.com>
2022-01-10 05:51:40 -0800

    small fix

--- GWAS_HAIL/4_build_pipelines.py
+++ GWAS_HAIL/4_build_pipelines.py
@@ -9,7 +9,7 @@ kt = hl.read_table(SAMPLE_QC_TABLE)
 n_samples = kt.count()
 n_samples
 
-PHESANT_FILE = "/mnt/i/UKB_DATA/imputed_UKB/hwpheno.tsv"
+PHESANT_FILE = "/mnt/i/UKB_DATA/normal/w/normal_lv_width.tsv"
 cols = list(pd.read_csv(PHESANT_FILE, sep='\t').columns)[1:]
 
 traits = hl.import_table(PHESANT_FILE,impute=True)
@@ -17,6 +17,6 @@ traits = traits.annotate(idx = hl.str(traits.idx))
 
 # join kt and traits by idx and sample
 table_joined = traits.key_by('idx').join(kt.key_by('eid'))
-table_joined.write("/mnt/i/UKBB_DATA/imputed_UKB/pipeline.kt",overwrite=True)
+table_joined.write("/mnt/i/UKB_DATA/imputed_UKB/pipeline_width.kt",overwrite=True)
 
 
